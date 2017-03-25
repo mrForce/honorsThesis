987d4f6046bce078b7578f5a8de051f42e8db610
Jordan Force <jordan.force@uconn.edu>
2017-03-25 17:32:50 -0400

    Finished the netchop script. I'm not going to commit everything else

--- netchop/chop.py
+++ netchop/chop.py
@@ -2,7 +2,7 @@ import sys
 from Bio import SeqIO
 import tempfile
 import subprocess
-
+import re
 """
 ERAP1 and 2 trim from left to right
 
@@ -22,15 +22,23 @@ def get_pieces(protein, chunk_width, netchop_output):
                 pieces.append(chunk)
 
 
-if len(sys.argv) == 3:
-    fasta_file = sys.argv[1]
+    return pieces
+
+if len(sys.argv) >= 4:
+    output_file = sys.argv[1]
+    
     chunk_width = int(sys.argv[2])
-    for seq_record in SeqIO.parse(fasta_file, "fasta"):
-        tf = tempfile.NamedTempFile('w+')
-        tf.write('>' + str(seq_record.id) + '\n')
-        tf.write(str(seq_record.seq))
-        output = subprocess.run(['netchop', '-m', 'netchop', '--threshold', '0.5', tf.name])
-        pieces = get_pieces(str(seq_record.seq), chunk_width, output)
-        for x in pieces:
-            print(x)
-        tf.close()
+    with open(output_file, 'w') as f:
+        for fasta_file in sys.argv[3::]:
+            for seq_record in SeqIO.parse(fasta_file, "fasta"):
+                tf = tempfile.NamedTemporaryFile('w+')
+                tf.write('>' + str(seq_record.id) + '\n')
+                tf.write(str(seq_record.seq))
+                tf.flush()
+                output = str(subprocess.run(['netchop', '-m', 'netchop', '--threshold', '0.5', tf.name], stdout=subprocess.PIPE, encoding='utf-8').stdout)
+                pieces = get_pieces(str(seq_record.seq), chunk_width, output)
+                for x in pieces:
+                    f.write(x + '\n')
+else:
+    print('usage: python chop.py output.txt chunk_width proteome1.fasta proteome2.fasta ...')
+            
